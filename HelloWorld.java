34641348156942521a788c15c47ffdf879a215dc
Kavidharshini <kavi14d@gmail.com>
2026-06-26 13:07:27 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub! This is my first code upload.");
+        
+        int number = 10;
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+    }
+}
